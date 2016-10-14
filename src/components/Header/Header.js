fa01196b8a16ed230d7cd4fc8216abf2bf0ce4d9
lqs-469 <529450714@qq.com>
2016-10-14 20:17:30 +0800

    :version:

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router'
 
 export const Header = () => (
   <div className='header-container'>
-    <h4><Link className='home-link' id='Home' to='/'>KMX疑难杂症 💉</Link></h4>
+    <h4><Link className='home-link' id='Home' to='/'>KMX疑难杂症 💉</Link>
+      <span className='header-right'>Beta 0.1</span>
+    </h4>
   </div>
 )
 
